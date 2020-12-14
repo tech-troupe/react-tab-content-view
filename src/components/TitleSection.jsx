781b557a87f4bb33e2e04e24493d9acf4dc6bfb9
eperiyasamy <eperiyasamy@paypal.com>
2020-12-14 14:20:06 -0800

    removed unused import to test storybook automation

--- src/components/TitleSection.jsx
+++ src/components/TitleSection.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import compose from 'recompose/compose';
 import Card from "@material-ui/core/Card";
